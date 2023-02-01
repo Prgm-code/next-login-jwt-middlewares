1010f0b332e4eff1cf3cd9544a5b6a996c8e6096
Patricio <pgomez.meneses@gmail.com>
2023-02-01 02:38:49 -0300

    first commit

--- middleware.js
+++ middleware.js
@@ -7,26 +7,26 @@ export async function middleware(request) {
   const jwt = request.cookies.get("myTokenName");
 
   // console.log(request.nextUrl.pathname);
-  if (request.nextUrl.pathname.includes("/dashboard")) {
-    if (jwt === undefined) {
-      console.log("token undefined");
-      return NextResponse.redirect(new URL("/login", request.url));
-    }
-    try {
-      const { payload } = await jwtVerify(
-        jwt.value,
-        new TextEncoder().encode(secret)
-      );
-      console.log(payload);
-      return NextResponse.next();
-    } catch (error) {
-      console.log(error, "token invalido");
 
-      console.log(request.nextUrl);
-      return NextResponse.redirect(new URL("/login", request.url));
-    }
-    console.log("la misma wea");
+  if (jwt === undefined) {
+    console.log("token undefined");
+    return NextResponse.redirect(new URL("/login", request.url));
   }
+  try {
+    const { payload } = await jwtVerify(
+      jwt.value,
+      new TextEncoder().encode(secret)
+    );
+    console.log(payload);
+    return NextResponse.next();
+  } catch (error) {
+    console.log(error, "token invalido");
 
-  return NextResponse.next();
+    console.log(request.nextUrl);
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+}
+
+export const config = {
+    matcher: ['/dashboard', '/profile'] // only apply to these paths
 }
